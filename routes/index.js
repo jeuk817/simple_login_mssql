8cf49dfd1e98c22570e1770a38bbfd8c0f30fdad
Ryujeuk <jeuk817@gmail.com>
2020-01-08 14:16:00 +0900

    feat: 회원가입 페이지 구현

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Simple Login' });
 });
+
+router.get('/signUpPage', (req, res, next) => {
+  res.render('signUpPage');
+})
 
 module.exports = router;
