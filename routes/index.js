1a77089de1589c3453659c2d8e9f0823246d985d
Ryujeuk <jeuk817@gmail.com>
2020-01-09 23:35:41 +0900

    feat: mssql server 연결 확인
    - 프로시저 공부필요

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const { connPoolPromise } = require('../db/connect_mssql');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Simple Login' });
@@ -10,8 +12,18 @@ router.get('/signUpPage', (req, res, next) => {
   res.render('signUpPage');
 });
 
-router.post('/signUp', (req, res, next) => {
+router.post('/signUp', async (req, res, next) => {
   const { id, pwd } = req.body;
+
+  try {
+    const pool = await connPoolPromise;
+    const result = await pool.request()
+      .query('select * from Users');
+
+    console.log(result);
+  } catch (err) {
+    return next(err);
+  }
 })
 
 module.exports = router;
