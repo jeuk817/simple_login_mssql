e4673e72d693b8426916e6c0d13faca379dc07ee
Ryujeuk <jeuk817@gmail.com>
2020-01-09 09:24:45 +0900

    feat: 회원가입 페이지로부터 id, pwd 넘어오는것 확인

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Simple Login' });
 });
 
 router.get('/signUpPage', (req, res, next) => {
   res.render('signUpPage');
+});
+
+router.post('/signUp', (req, res, next) => {
+  const { id, pwd } = req.body;
 })
 
 module.exports = router;
